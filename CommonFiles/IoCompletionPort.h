9c520c852a2c7e6aa884fbf58fb082cbc314d372
man <ozziewei@gmail.com>
2023-07-26 00:02:45 +0800

    format

--- CommonFiles/IoCompletionPort.h
+++ CommonFiles/IoCompletionPort.h
@@ -5,75 +5,68 @@ Purpose: This class wraps an I/O Completion Port.
          See Appendix B.
 ******************************************************************************/
 
-
-#pragma once   // Include this header file once per compilation unit
-
+#pragma once // Include this header file once per compilation unit
 
 ///////////////////////////////////////////////////////////////////////////////
 
-
-#include "..\CommonFiles\CmnHdr.h"              // See Appendix A.
-
+#include "..\CommonFiles\CmnHdr.h" // See Appendix A.
 
 ///////////////////////////////////////////////////////////////////////////////
-
 
 class CIOCP {
 public:
-   CIOCP(int nMaxConcurrency = -1) { 
-      m_hIOCP = NULL; 
-      if (nMaxConcurrency != -1)
-         (void) Create(nMaxConcurrency);
-   }
+  CIOCP(int nMaxConcurrency = -1) {
+    m_hIOCP = NULL;
+    if (nMaxConcurrency != -1)
+      (void)Create(nMaxConcurrency);
+  }
 
-   ~CIOCP() { 
-      if (m_hIOCP != NULL) 
-         chVERIFY(CloseHandle(m_hIOCP)); 
-   }
+  ~CIOCP() {
+    if (m_hIOCP != NULL)
+      chVERIFY(CloseHandle(m_hIOCP));
+  }
 
+  BOOL Close() {
+    BOOL bResult = CloseHandle(m_hIOCP);
+    m_hIOCP = NULL;
+    return (bResult);
+  }
 
-   BOOL Close() {
-      BOOL bResult = CloseHandle(m_hIOCP);
-      m_hIOCP = NULL;
-      return(bResult);
-   }
+  BOOL Create(int nMaxConcurrency = 0) {
+    m_hIOCP =
+        CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, nMaxConcurrency);
+    chASSERT(m_hIOCP != NULL);
+    return (m_hIOCP != NULL);
+  }
 
-   BOOL Create(int nMaxConcurrency = 0) {
-      m_hIOCP = CreateIoCompletionPort(
-         INVALID_HANDLE_VALUE, NULL, 0, nMaxConcurrency);
-      chASSERT(m_hIOCP != NULL);
-      return(m_hIOCP != NULL);
-   }
+  BOOL AssociateDevice(HANDLE hDevice, ULONG_PTR CompKey) {
+    BOOL fOk =
+        (CreateIoCompletionPort(hDevice, m_hIOCP, CompKey, 0) == m_hIOCP);
+    chASSERT(fOk);
+    return (fOk);
+  }
 
-   BOOL AssociateDevice(HANDLE hDevice, ULONG_PTR CompKey) {
-      BOOL fOk = (CreateIoCompletionPort(hDevice, m_hIOCP, CompKey, 0) 
-         == m_hIOCP);
-      chASSERT(fOk);
-      return(fOk);
-   }
+  BOOL AssociateSocket(SOCKET hSocket, ULONG_PTR CompKey) {
+    return (AssociateDevice((HANDLE)hSocket, CompKey));
+  }
 
-   BOOL AssociateSocket(SOCKET hSocket, ULONG_PTR CompKey) {
-      return(AssociateDevice((HANDLE) hSocket, CompKey));
-   }
+  BOOL PostStatus(ULONG_PTR CompKey, DWORD dwNumBytes = 0,
+                  OVERLAPPED *po = NULL) {
 
-   BOOL PostStatus(ULONG_PTR CompKey, DWORD dwNumBytes = 0, 
-      OVERLAPPED* po = NULL) {
+    BOOL fOk = PostQueuedCompletionStatus(m_hIOCP, dwNumBytes, CompKey, po);
+    chASSERT(fOk);
+    return (fOk);
+  }
 
-      BOOL fOk = PostQueuedCompletionStatus(m_hIOCP, dwNumBytes, CompKey, po);
-      chASSERT(fOk);
-      return(fOk);
-   }
+  BOOL GetStatus(ULONG_PTR *pCompKey, PDWORD pdwNumBytes, OVERLAPPED **ppo,
+                 DWORD dwMilliseconds = INFINITE) {
 
-   BOOL GetStatus(ULONG_PTR* pCompKey, PDWORD pdwNumBytes,
-      OVERLAPPED** ppo, DWORD dwMilliseconds = INFINITE) {
-
-      return(GetQueuedCompletionStatus(m_hIOCP, pdwNumBytes, 
-         pCompKey, ppo, dwMilliseconds));
-   }
+    return (GetQueuedCompletionStatus(m_hIOCP, pdwNumBytes, pCompKey, ppo,
+                                      dwMilliseconds));
+  }
 
 private:
-   HANDLE m_hIOCP;
+  HANDLE m_hIOCP;
 };
-
 
 ///////////////////////////////// End of File /////////////////////////////////
